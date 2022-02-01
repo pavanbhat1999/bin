e2260e83175918e5e2c05972b2b85e70c4a5c715
pavanbhat1999 <prbhat07@gmail.com>
2022-02-01 11:32:00 +0530

    update

--- themes/change-wallpaper.sh
+++ themes/change-wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# nitrogen --set-auto --no-recurse --random $HOME/Downloads/Wall/
-DISPLAY=:0 feh --bg-scale --randomize $HOME/Downloads/Wall/
+nitrogen --set-auto --no-recurse --random $HOME/Downloads/Wall/
+# DISPLAY=:0 feh --bg-scale --randomize $HOME/Downloads/Wall/
