efee6f5abc1cbb730ea29e3ea7c5a6fb9282bdc2
pavanbhat1999 <prbhat07@gmail.com>
2022-02-14 13:34:02 +0530

    Minor change in wallheaven script

--- themes/wallheaven.sh
+++ themes/wallheaven.sh
@@ -5,7 +5,7 @@ export DISPLAY=:0
 export XAUTHORITY=/home/root99/.config/Xauthority
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
- image_url=$(curl https://wallhaven.cc/api/v1/search\?q\=anime\&sorting\=random\&atleast\&resolutions\=1920x1080 | jq '.' | grep -Eoh "https:\/\/w.wallhaven.cc\/full\/.*(jpg|png)\b" | head -1)
+ image_url=$(curl https://wallhaven.cc/api/v1/search\?q\=anime\&sorting\=random\&atleast\&resolutions\=1920x1080\&colors\=000000\&atleast\=1920x1080 | jq '.' | grep -Eoh "https:\/\/w.wallhaven.cc\/full\/.*(jpg|png)\b" | head -1)
 
  store_directory=$HOME/Downloads/tmpWall/
  wget -P $store_directory $image_url
